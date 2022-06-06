2db973f5b19b554231f3791e7eb918c068ed5465
JJ Jerry <j.kociubinski93@gmail.com>
2022-06-06 19:26:43 +0200

    Set up Main Menu

---
+++ Programming Theory/Assets/Scripts/MainManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+
+    // ENCAPSULATION
+    public static MainManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
